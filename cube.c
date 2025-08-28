82adcf5e544b2e20555daa5a3a2610cb60722f2f
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2025-08-29 02:17:45 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    //cube of n 
+    int n;
+    printf("enter value of n\n");
+    scanf("%d", &n);
+    printf("cube of n number is %d", n*n*n);
+    return 0;
+}
