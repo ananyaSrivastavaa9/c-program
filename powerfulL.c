4e6051fb3419202791af8b311b1dcf98e7302d3c
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2025-08-29 02:19:13 +0530

    Add files via upload

---
+++ powerfulL.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    printf("C is a powerful language\n");
+    printf("C is a powerful language\n");
+    printf("C is a powerful language\n");
+    printf("C is a powerful language\n");
+    printf("C is a powerful language\n");
+    return 0;
+}
