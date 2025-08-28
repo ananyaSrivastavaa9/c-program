d28afbcbe185e39349a4dc53d126822b59a74a2a
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2025-08-29 02:21:02 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int x,y;
+   printf("Enter number x\n");
+   scanf("%d", &x);
+   printf("enter the number y\n");
+   scanf("%d", &y);
+   printf("the sum is %d\n", x+y);
+ 
+    return 0;
+}
